de090325363068cef0af761af4788cec901b924d
dlsrud0108 <dlsrud0108@yonsei.ac.kr>
2024-01-18 17:18:54 +0900

    success

--- main.py
+++ main.py
@@ -4,28 +4,44 @@ import base64
 import io
 import requests
 from transformers import BlipProcessor, BlipForConditionalGeneration
+from typing import Union
+from fastapi import FastAPI, UploadFile
 
-app = Flask(__name__)
+app = FastAPI()
+
+
+# app = Flask(__name__)
 def process_image_with_cpu_model(image):
-    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
-    #raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+    processor = BlipProcessor.from_pretrained("./model")
+    model = BlipForConditionalGeneration.from_pretrained("./model")
+    # raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
     inputs = processor(image, return_tensors="pt")
-    out = model.generate(**inputs)  
-    return out
+    out = model.generate(**inputs)
+    answer = processor.decode(out[0], skip_special_tokens=True)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    return answer
 
-@app.route('/upload', methods=['POST'])
-def upload():
+
+# @app.get('/')
+# def index():
+#     return render_template('index.html')
+
+
+@app.post("/upload")
+async def create_upload_file(file: UploadFile):
     try:
-        image_data = request.form['image'].split(',')[1]
-        image = Image.open(io.BytesIO(base64.b64decode(image_data)))
+        print("test")
+        # image_data = request.form['image'].split(',')[1]
+        image_data = await file.read()
+        print("Received image data:", image_data)
+        image = Image.open(io.BytesIO(image_data))
+        # image = Image.open(io.BytesIO(base64.b64decode(image_data)))
         result_caption = process_image_with_cpu_model(image)
-        return jsonify({"caption": result_caption})
+        return result_caption
     except Exception as e:
-        return jsonify({"error": str(e)})
-if __name__ == '__main__':
+        print("Error:", str(e))
+        return str(e)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
